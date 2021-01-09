403ed729879b36ba885c0dff52f7657e1f36c982
Daniel Lozano Navas <d.lozanonavas@outlook.com>
2021-01-09 01:18:38 -0500

    Refactor: Namespaces & Types

--- Extensions/AllianceBusinessModelExtensions.cs
+++ Extensions/AllianceBusinessModelExtensions.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using FenixAlliance.ABM.Data;
-using FenixAlliance.Data;
+using FenixAlliance.ACL.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using FenixAlliance.Options;
 
 namespace FenixAlliance.ABM.Hub.Extensions
 {
