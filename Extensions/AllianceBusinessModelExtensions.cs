dc4f52245fc41750670dc55a30172f2c06ba78c3
Daniel Lozano Navas <d.lozanonavas@outlook.com>
2021-01-12 11:54:15 -0500

    v1.1.1

--- Extensions/AllianceBusinessModelExtensions.cs
+++ Extensions/AllianceBusinessModelExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 using FenixAlliance.ABM.Data;
-using FenixAlliance.ACL.Configuration;
+using FenixAlliance.ACL.Configuration.Types;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,11 +11,9 @@ namespace FenixAlliance.ABM.Hub.Extensions
 {
     public static class AllianceBusinessModelExtensions
     {
-        public static void AddAllianceBusinessModelServices(this IServiceCollection services, IConfiguration Configuration, IHostEnvironment Environment, PlatformOptions Options)
+        public static void AddAllianceBusinessModelServices(this IServiceCollection services, IConfiguration Configuration, IHostEnvironment Environment, SuiteOptions Options)
         {
-
-
-                switch (Options.Functionalities.AllianceBusinessModel.Provider)
+                switch (Options.ABM.Provider)
                 {
                     case "MSSQL":
                         // Use Production MSSQL DB
@@ -37,9 +35,9 @@ namespace FenixAlliance.ABM.Hub.Extensions
         }
 
 
-        public static void AddMySQL(this IServiceCollection services, IConfiguration Configuration, IHostEnvironment Environment, PlatformOptions Options, bool Development)
+        public static void AddMySQL(this IServiceCollection services, IConfiguration Configuration, IHostEnvironment Environment, SuiteOptions Options, bool Development)
         {
-            var Provider = Options.Functionalities.AllianceBusinessModel.Providers.Last(c =>
+            var Provider = Options.ABM.Providers.Last(c =>
                 c.Name == "MSSQL" && c.Purpose == "ABM.Data" && c.Environment == ((!Development) ? "Production" : "Development"));
 
             // Use Development MySQL DB
@@ -53,9 +51,9 @@ namespace FenixAlliance.ABM.Hub.Extensions
         }
 
         public static void AddMSSQL(this IServiceCollection services,
-            IConfiguration Configuration, IHostEnvironment Environment, PlatformOptions Options, bool Development)
+            IConfiguration Configuration, IHostEnvironment Environment, SuiteOptions Options, bool Development)
         {
-            var Provider = Options.Functionalities.AllianceBusinessModel.Providers.Last(c =>
+            var Provider = Options.ABM.Providers.Last(c =>
                 c.Name == "MSSQL" && c.Purpose == "ABM.Data" && c.Environment == ((!Development) ? "Production" : "Development"));
 
             // Use Development MSSQL DB
